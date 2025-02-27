f127dc096d736f35667c18563ef5bc4e3b51bf38
tangs <tangsong1988kaka@gmail.com>
2025-02-27 12:02:47 +0800

    Refactored CMakeLists.txt to reorder time2 and add time1 and time3 executables.

---
+++ src/time3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <asio.hpp>
+#include <functional>
+#include "utils/time_utils.h"
+
+static void print(const std::error_code& /*e*/,
+    asio::steady_timer* t, int* count) {
+      if (*count < 5)  {
+          std::cout << "[" << utils::get_current_time() << "] "
+                    << "count:" << *count << std::endl;
+          ++(*count);
+
+          t->expires_at(t->expiry() + asio::chrono::seconds(1));
+          t->async_wait(std::bind(print,
+                asio::placeholders::error, t, count));
+      }
+}
+
+int main() {
+    asio::io_context io;
+
+    int count = 0;
+    asio::steady_timer t(io, asio::chrono::seconds(1));
+    t.async_wait(std::bind(print, asio::placeholders::error, &t, &count));
+
+    io.run();
+
+    std::cout << "[" << utils::get_current_time() << "] "
+              << "Final count is " << count << std::endl;
+    return 0;
+} 
